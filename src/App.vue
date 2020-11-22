9141781052eccfbea6bb3c66d46ccf26f8ba598b
pong10 <pong.1999@hotmail.com>
2020-11-23 01:52:12 +0700

    Export store for all pages

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,11 @@
 </template>
 
 <script>
+  import store from './store/store'
   // Loading some plugin css asynchronously
   import('sweetalert2/dist/sweetalert2.css')
   import('vue-notifyjs/themes/default.css')
-  export default {}
+  export default {store}
 </script>
 <style lang="scss">
   .notifications.vue-notifyjs {
